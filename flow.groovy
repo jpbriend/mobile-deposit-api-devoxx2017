dc4fc3f5afd440c70205741838b6b11f990c03d9
Kurt Madel <kmadel@mac.com>
2015-06-15 17:16:31 -0400

    updated pom and flow to suppore release version

--- flow.groovy
+++ flow.groovy
@@ -4,7 +4,12 @@ stage 'build'
 node('docker') {
     docker.withServer('tcp://127.0.0.1:1234'){
             docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
+                sh 'rm -rf *'
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-api.git']]])
+                sh 'git checkout master'
+                sh 'git config user.email "kmadel@mac.com"'
+                sh 'git config user.name "kmadel"'
+                sh 'git remote set-url origin git@github.com:cloudbees/mobile-deposit-api.git'
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
                 stage 'integration-test'
@@ -18,12 +23,12 @@ node('docker') {
                     echo "Releaed version ${buildVersion}"
                 }
                 matcher = null
-                archive "target/mobile-deposit-api.jar, target/Dockerfile"
+                archive "target/*.jar, target/Dockerfile"
             }
 
   docker.withServer('tcp://54.173.235.97:2375'){
 
-        unarchive mapping: ['target/mobile-deposit-api.jar' : '.', 'target/Dockerfile' : '.']
+        unarchive mapping: ['target/': '.']
         stage 'build docker image'
         docker.build "kmadel/mobile-deposit-api:${dockerBuildTag}"
 
@@ -34,11 +39,11 @@ node('docker') {
         } catch (Exception _) {
            echo "no container to stop"        
         }
-        sh "docker run -d --name mobile-deposit-api -p 8080:8080 kmadel/mobile-deposit-api:${dockerBuildTag}"
+        sh "docker run -d --name mobile-deposit-api -p 8080:8080 kmadel/mobile-deposit-api:${buildVersion}"
         //hack to kick off ui job
         //sh 'curl "http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/job/mobile-deposit-ui-workflow/build?token=llNSDXpfTim4Bm2SIIoQezwwQOHmEMYgSeHSUnL"'
         stage 'publish docker image'
-        sh 'curl -H "Content-Type: application/json" -X POST -d \'{"push_data": {"pushed_at": 1434386606, "images": null, "pusher": "kmadel"}, "callback_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/hook/21a0ic0dje2ff4hg3f3hbg23b5220454b/", "repository": {"status": "Active", "description": "", "is_trusted": false, "full_description": "", "repo_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/", "owner": "kmadel", "is_official": false, "is_private": false, "name": "mobile-bank-api", "namespace": "kmadel", "star_count": 0, "comment_count": 0, "date_created": 1434385021, "repo_name": "kmadel/mobile-bank-api"}}\' http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/dockerhub-webhook/notify'
+        //sh 'curl -H "Content-Type: application/json" -X POST -d \'{"push_data": {"pushed_at": 1434386606, "images": null, "pusher": "kmadel"}, "callback_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/hook/21a0ic0dje2ff4hg3f3hbg23b5220454b/", "repository": {"status": "Active", "description": "", "is_trusted": false, "full_description": "", "repo_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/", "owner": "kmadel", "is_official": false, "is_private": false, "name": "mobile-bank-api", "namespace": "kmadel", "star_count": 0, "comment_count": 0, "date_created": 1434385021, "repo_name": "kmadel/mobile-bank-api"}}\' http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/dockerhub-webhook/notify'
    }
   }
 }
