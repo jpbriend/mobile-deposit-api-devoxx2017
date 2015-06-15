2bb34fe8827fa76d41706ee789476fab31f5d32c
Kurt Madel <kmadel@mac.com>
2015-06-15 10:06:21 -0400

    updated flow - removed image push as not working on api-team master

--- flow.groovy
+++ flow.groovy
@@ -20,17 +20,28 @@ node('docker') {
                 matcher = null
                 archive "target/mobile-deposit-api.jar, target/Dockerfile"
             }
-    }
+
+  docker.withServer('tcp://54.173.235.97:2375'){
+    docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
         unarchive mapping: ['target/mobile-deposit-api.jar' : '.', 'target/Dockerfile' : '.']
         stage 'build docker image'
         def mobileDepositApiImage = docker.build "kmadel/mobile-deposit-api:${dockerBuildTag}"
 
         stage 'deploy to production'
-        sh "docker run -d --name mobile-deposit-api -p 8080:8080 mobile-deposit-api:${dockerBuildTag}"
+        try{
+          sh "docker stop mobile-deposit-api"
+          sh "docker rm mobile-deposit-api"
+        } catch (Exception _) {
+           echo "no container to stop"        
+        }
+        sh "docker run -d --name mobile-deposit-api -p 8080:8080 kmadel/mobile-deposit-api:${dockerBuildTag}"
 
         stage 'publish docker image'
-        docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
-            //mobileDepositApiImage.push()
-            mobileDepositApiImage.push "${dockerBuildTag}"
-        }
+        //sh 'docker login -e "kmadel@mac.com" -u "kmadel" -p "noah2772"'
+        //mobileDepositApiImage.push "${dockerBuildTag}"
+       sh 'curl "http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/job/mobile-deposit-ui-workflow/build?token=llNSDXpfTim4Bm2SIIoQezwwQOHmEMYgSeHSUnL"'
+    }
+   }
+
+  }
 }
