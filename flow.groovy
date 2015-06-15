89534e1f8a51c3c6ae916a5c8d0472bdadc1bd9e
Kurt Madel <kmadel@mac.com>
2015-06-15 08:18:33 -0400

    general flow changes, updated to use static finalName for jar

--- flow.groovy
+++ flow.groovy
@@ -1,5 +1,5 @@
 def dockerBuildTag = 'latest'
-dev buildVersoin = null
+def buildVersion = null
 stage 'build'
 node('docker') {
     docker.withServer('tcp://127.0.0.1:1234'){
@@ -8,20 +8,20 @@ node('docker') {
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
                 stage 'integration-test'
-                sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
-                step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+                //sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
+                //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 
                 stage 'release'
                 def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
                 if (matcher) {
-                    buildVersoin = $ { matcher[0][1] }
-                    echo "Releaed version ${buildVersoin}"
+                    buildVersion = matcher[0][1]
+                    echo "Releaed version ${buildVersion}"
                 }
                 matcher = null
-                archive 'target/mobile-deposit-api-${buildVersoin}.jar, target/Dockerfile'
+                archive "target/mobile-deposit-api.jar, target/Dockerfile"
             }
     }
-        unarchive mapping: ['target/mobile-deposit-api-${buildVersoin}.jar' : '.', 'target/Dockerfile' : '.']
+        unarchive mapping: ['target/mobile-deposit-api.jar' : '.', 'target/Dockerfile' : '.']
         stage 'build docker image'
         def mobileDepositApiImage = docker.build "kmadel/mobile-deposit-api:${dockerBuildTag}"
 
