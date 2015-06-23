78372851ec9f1eac942eb7420d4d77cb2c995fa0
Kurt Madel <kmadel@mac.com>
2015-06-23 09:26:35 -0700

    added checkpoint

--- flow.groovy
+++ flow.groovy
@@ -11,7 +11,12 @@ node('docker') {
                 sh 'git remote set-url origin git@github.com:cloudbees/mobile-deposit-api.git'
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
+                archive 'pom.xml, src/'
+
+                checkpoint 'Finished build'
+
                 stage 'sonar analysis'
+                unarchive mapping: ['pom.xml' : '.', 'src/' : '.']
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo sonar:sonar'
 
                 stage 'integration-test'
@@ -44,7 +49,7 @@ node('docker') {
         }
         mobileDepositApiImage.run("--name mobile-deposit-api -p 8080:8080")
 
-        stage 'publish docker image'
+      stage 'publish docker image'
         sh 'curl -H "Content-Type: application/json" -X POST -d \'{"push_data": {"pushed_at": 1434386606, "images": null, "pusher": "kmadel"}, "callback_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/hook/21a0ic0dje2ff4hg3f3hbg23b5220454b/", "repository": {"status": "Active", "description": "", "is_trusted": false, "full_description": "", "repo_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/", "owner": "kmadel", "is_official": false, "is_private": false, "name": "mobile-bank-api", "namespace": "kmadel", "star_count": 0, "comment_count": 0, "date_created": 1434385021, "repo_name": "kmadel/mobile-bank-api"}}\' http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/dockerhub-webhook/notify'
    }
   }
