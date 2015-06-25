e7f10ab87e9ee116d8947a01cd75bbdc956ed31e
Kurt Madel <kmadel@mac.com>
2015-06-25 15:28:41 -0400

    updated sonar command

--- flow.groovy
+++ flow.groovy
@@ -11,7 +11,8 @@ node('docker') {
             sh 'git remote set-url origin git@github.com:cloudbees/mobile-deposit-api.git'
             sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
-            sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo sonar:sonar'
+            stage 'sonar analysis'
+            sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo -Dsonar.scm.disabled=True sonar:sonar'
 
             stage 'integration-test'
             sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
