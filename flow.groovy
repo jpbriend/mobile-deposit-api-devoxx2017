b5e296e0f11bf0954511fb9e86d7d0aaf7c4171e
Kurt Madel <kmadel@mac.com>
2015-07-15 11:09:59 -0700

    added checkpoints and paralell step for quality analysis

--- flow.groovy
+++ flow.groovy
@@ -1,5 +1,5 @@
 def buildVersion = null
-stage 'build'
+stage 'Build'
 node('docker') {
     docker.withServer('tcp://127.0.0.1:1234') {
         docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
@@ -10,33 +10,56 @@ node('docker') {
             sh 'git config user.name "kmadel"'
             sh 'git remote set-url origin git@github.com:cloudbees/mobile-deposit-api.git'
             sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
+            archive 'pom.xml, src/, target/'
 
-            stage 'sonar analysis'
-            sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo -Dsonar.scm.disabled=True sonar:sonar'
-
-            stage 'integration-test'
-            sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
-            //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-
-            stage 'prepare release'
-            def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
-            if (matcher) {
-                buildVersion = matcher[0][1]
-                echo "Released version ${buildVersion}"
-            }
-            matcher = null
+            
+            
         }
     }
+}
 
+checkpoint 'Build Complete'
+
+stage 'Quality Analysis'
+node('docker') {
+    unarchive mapping: ['pom.xml' : '.', 'src/' : '.']
+    docker.withServer('tcp://127.0.0.1:1234') {
+        parallel(
+            integrationTests: {
+              docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
+                  sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
+              }
+            }, sonarAnalysis: {
+                docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
+                  sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo -Dsonar.scm.disabled=True sonar:sonar'
+                }
+            }, failFast: true
+        )
+    }
+}
+
+checkpoint 'Quality Analysis Complete'
+
+node('docker') {
+    unarchive mapping: ['pom.xml' : '.', 'target/' : '.']
+
+    stage 'Version Release'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    if (matcher) {
+        buildVersion = matcher[0][1]
+        echo "Release version: ${buildVersion}"
+    }
+    matcher = null
+    
     docker.withServer('tcp://54.165.201.3:2376', 'slave-docker-us-east-1-tls'){
 
-        stage 'build docker image'
+        stage 'Build Docker Image'
         def mobileDepositApiImage
         dir('target') {
             mobileDepositApiImage = docker.build "kmadel/mobile-deposit-api:${buildVersion}"
         }
 
-        stage 'deploy to production'
+        stage 'Deploy to Prod'
         try{
           sh "docker stop mobile-deposit-api"
           sh "docker rm mobile-deposit-api"
@@ -45,8 +68,8 @@ node('docker') {
         }
         mobileDepositApiImage.run("--name mobile-deposit-api -p 8080:8080")
         sh 'curl http://webhook:336838a2daad1ea4ed0d18734ff6a9fb@jenkins.beedemo.net/api-team/docker-traceability/submitContainerStatus --data-urlencode inspectData="$(docker inspect mobile-deposit-api)" --data-urlencode hostName=prod-server-1'
-
-        stage 'publish docker image'
+        
+        stage 'Publish Docker Image'
         docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
             mobileDepositApiImage.push()
         }
