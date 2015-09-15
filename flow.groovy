fd3bfa4564275ed4461c68ab97274193dc660f64
Kurt Madel <kmadel@mac.com>
2015-09-15 18:01:29 -0400

    updated traceability call

--- flow.groovy
+++ flow.groovy
@@ -85,7 +85,7 @@ node('docker') {
            echo "no container to stop"        
         }
         mobileDepositApiImage.run("--name mobile-deposit-api -p 8080:8080")
-        sh 'curl http://webhook:336838a2daad1ea4ed0d18734ff6a9fb@jenkins.beedemo.net/api-team/docker-traceability/submitContainerStatus --data-urlencode inspectData="$(docker inspect mobile-deposit-api)" --data-urlencode hostName=prod-server-1'
+        sh 'curl http://webhook:336838a2daad1ea4ed0d18734ff6a9fb@jenkins.beedemo.net/api-team/docker-traceability/submitContainerStatus --data-urlencode status=deployed --data-urlencode inspectData="$(docker inspect mobile-deposit-api)" --data-urlencode hostName=prod-server-1 --data-urlencode hostName=prod --data-urlencode imageName=cloudbees/mobile-deposit-api'
         
         stage 'Publish Docker Image'
         docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
