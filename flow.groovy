36daafca277fcb5b10b0ec4a34dd56445a41fa8d
Kurt Madel <kmadel@mac.com>
2015-10-05 10:08:45 -0400

    Update flow.groovy
    Possible fix for sonar properties issue:
    
    [ERROR] [ERROR] Some problems were encountered while processing the POMs:
    [ERROR] Resolving expression: '${sonar.jdbc.password}': Detected the following recursive expression cycle in 'sonar.jdbc.password': [sonar.jdbc.password] @ 
    [ERROR] Resolving expression: '${sonar.jdbc.username}': Detected the following recursive expression cycle in 'sonar.jdbc.username': [sonar.jdbc.username] @ 
     @

--- flow.groovy
+++ flow.groovy
@@ -8,7 +8,7 @@ node('docker-cloud') {
     //docker.withServer('tcp://127.0.0.1:1234') {
         docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
             checkout([$class: 'GitSCM', branches: [[name: '*/master']], clean: true, doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-api.git']]])
-            sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
+            sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo  -Dsonar.jdbc.username=NULL -Dsonar.jdbc.password=NULL clean package'
         }
     //}
     archive 'pom.xml, src/, target/'
